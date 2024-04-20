f9d2d71999414133136da9bc286dafb69fa5a24d
rafael1209 <mr.chasmanrafael@gmail.com>
2024-04-21 00:08:23 +0300

    Fixed GetSkinPart bug. When users from russia cant see a skins

---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+using spworlds;
+using System.Text.Json.Nodes;
+namespace spworlds.Types;
+
+public class User
+{
+        public string Name { get; }
+
+        public string Uuid { get; }
+
+        public bool IsPlayer() => Name != null ? true : false;
+
+        public User(string name, string uuid)
+        {
+                Name = name;
+                Uuid = uuid;
+        }
+
+        public static async Task<User> CreateUser(string name)
+        {
+                string uuid;
+                using (HttpClient client = new())
+                {
+                        uuid = (string)JsonNode.Parse(await client.GetStringAsync($"https://api.mojang.com/users/profiles/minecraft/{name}"))["id"];
+                }
+                User user = new(name, uuid);
+                return user;
+        }
+
+        public string GetSkinPart(SkinPart skinPart, string size = "64")
+        {
+                return (string)$"https://avatar.spworlds.ru/{skinPart}/{size}/{this.Uuid}";
+        }
+}
